2a5349906e8a3e2c8caee2bcf58f30ab7ad2cc03
Shan <92211264+sannidhyasahoo@users.noreply.github.com>
2021-11-28 19:01:04 +0530

    Add files via upload

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -1,31 +1,36 @@
-#BMI_Calculator made by SANNIDHYA SAHOO, 11191
-start=(input("Hi There. This is BMI Calculator.\nThis program is build on Python by Sannidhya Sahoo.\n ENTER 1 TO CONTINUE :: "))
+#BMI_Calculator made by SANNIDHYA SAHOO
+name=(input("What is your name::"))
+name=name.strip()
+start=(input("Hi There. This is BMI Calculator.\nThis program is build on Python by Sannidhya Sahoo.\n ENTER 1 TO CONTINUE::"))
+n=1
 if start=='1':
-          height=int(input('Enter your height (in cms)::'))
-          weight=int(input('Enter your weight (in kgs)::'))
-          BMI=weight/(height/100)**2
-          print('Your BMI is',BMI)
-          if BMI<18.5:
-              print("You are underweight.")
-          elif BMI>=18.5 and BMI<25.0:
-              print('Your weight is normal')
-          elif BMI>=25 and BMI<30:
-              print("You are Overweight")
-          elif BMI>=30 and BMI<35:
-              print("You are having Obesity (Class I)")
-          elif BMI>=35 and BMI<40:
-              print("You are having Obesity (Class II)")
-          elif BMI>=40:
-              print("You are having Obesity (Class III)")
-          else:
-              print("INVALID INPUT(S) !!! .\nPROGRAM TERMINATED !!!")
-ChoicOApp=int(input("Enter 2 if you would like to download \nBMI Calculator App \nOr Enter 3 to view the source code \nENTER YOUR CHOICE::"))
+    height=int(input('Enter your height (in cms)::'))
+    weight=int(input('Enter your weight (in kgs)::'))
+    BMI=weight/(height/100)**2
+    print('\n\tYour BMI is',BMI)
+    if BMI<18.5:
+        print("\n\t＞︿＜",name," you are underweight.\n")
+    elif BMI>=18.5 and BMI<25.0:
+        print('\n\to*￣▽￣*o',name,', your weight is normal\n')
+    elif BMI>=25 and BMI<30:
+        print("\n\t",name,", you are Overweight\n")
+    elif BMI>=30 and BMI<35:
+        print("\n\t",name,", you are having Obesity (Class I)\n")
+    elif BMI>=35 and BMI<40:
+        print("\n\t",name,",you are having Obesity (Class II)\n")
+    elif BMI>=40:
+        print("\n\t",name,", you are having Obesity (Class III)\n")
+    else:
+        print("INVALID INPUT(S) !!! .\nPROGRAM TERMINATED !!!")
+ChoicOApp=int(input("\t^_^ Enter 2 if you would like to download BMI Calculator App \n\tOr Enter 3 to view the source code \n\tENTER YOUR CHOICE::"))
 import webbrowser
 if  ChoicOApp==2:
     print("Opening Browser...")
-    print("Oprning https://github.com/sannidhyasahoo/SensApp/raw/main/Sensors.apk ...")
+    print("https://github.com/sannidhyasahoo/SensApp/raw/main/Sensors.apk")
     webbrowser.open("https://github.com/sannidhyasahoo/SensApp/raw/main/Sensors.apk")
 elif ChoicOApp==3:
     print("Opening Browser...")
-    print("https://github.com/sannidhyasahoo/BMI_Py/blob/main/BMI_Calculator.py")
+    print("https://github.com/sannidhyasahoo/BMI_Py/blob/main/BMI_Calculator.py") 
     webbrowser.open("https://github.com/sannidhyasahoo/BMI_Py/blob/main/BMI_Calculator.py")
+print("if your browser doesn`t open c2opy the above\nlink and paste it on your browser")
+print("========================================================================")
