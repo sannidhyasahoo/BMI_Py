365f76d056ee365decac4cca61cfc090de57dfae
Shan <92211264+sannidhyasahoo@users.noreply.github.com>
2021-12-02 12:07:07 +0530

    Add files via upload

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -1,7 +1,7 @@
-#BMI_Calculator made by SANNIDHYA SAHOO
+#BMI_Calculator made by SANNIDHYA SAHOO,11191
 name=(input("What is your name::"))
 name=name.strip()
-start=(input("Hi There. This is BMI Calculator.\nThis program is build on Python by Sannidhya Sahoo.\n ENTER 1 TO CONTINUE::"))
+start=(input("Hi there. This is BMI Calculator.\nThis program is build on Python by Sannidhya Sahoo.\n ENTER 1 TO CONTINUE::"))
 n=1
 if start=='1':
     height=int(input('Enter your height (in cms)::'))
@@ -13,16 +13,16 @@ if start=='1':
     elif BMI>=18.5 and BMI<25.0:
         print('\n\to*￣▽￣*o',name,', your weight is normal\n')
     elif BMI>=25 and BMI<30:
-        print("\n\t",name,", you are Overweight\n")
+        print("\n\t＞﹏＜",name,", you are Overweight\n")
     elif BMI>=30 and BMI<35:
-        print("\n\t",name,", you are having Obesity (Class I)\n")
+        print("\n\t⊙﹏⊙∥",name,", you are having Obesity (Class I)\n")
     elif BMI>=35 and BMI<40:
-        print("\n\t",name,",you are having Obesity (Class II)\n")
+        print("\n\t{{{>_<}}}",name,",you are having Obesity (Class II)\n")
     elif BMI>=40:
-        print("\n\t",name,", you are having Obesity (Class III)\n")
+        print("\n\t⊙_⊙;",name,", you are having Obesity (Class III)\n")
     else:
         print("INVALID INPUT(S) !!! .\nPROGRAM TERMINATED !!!")
-ChoicOApp=int(input("\t^_^ Enter 2 if you would like to download BMI Calculator App \n\tOr Enter 3 to view the source code \n\tENTER YOUR CHOICE::"))
+ChoicOApp=int(input("\t^_^ Enter 2 if you would like to download \n\tBMI Calculator App for Android \n\tOr Enter 3 to view the source code \n\tENTER YOUR CHOICE::"))
 import webbrowser
 if  ChoicOApp==2:
     print("Opening Browser...")
@@ -32,5 +32,5 @@ elif ChoicOApp==3:
     print("Opening Browser...")
     print("https://github.com/sannidhyasahoo/BMI_Py/blob/main/BMI_Calculator.py") 
     webbrowser.open("https://github.com/sannidhyasahoo/BMI_Py/blob/main/BMI_Calculator.py")
-print("if your browser doesn`t open c2opy the above\nlink and paste it on your browser")
+print("if your browser doesn`t open copy the above\nlink and paste it on your browser")
 print("========================================================================")
